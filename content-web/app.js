741d33fbf86b85f5c09f55949334cfc94dc4d03a
Reed88 <jonathan.reed@milliman.com>
2022-03-15 19:38:27 +0000

    Added Application Insights

--- content-web/app.js
+++ content-web/app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const app = express();
 
 const appInsights = require("applicationinsights");
-appInsights.setup("[82f95c63-7691-467d-8503-24465b54e859]");
+appInsights.setup("82f95c63-7691-467d-8503-24465b54e859");
 appInsights.start();
 
 app.use(express.static(path.join(__dirname, 'dist/content-web')));
